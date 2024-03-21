207b9b14479b0be14037dcee39c8eb166b689b52
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-21 14:52:56 +0600

    update navbar task

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './Navbar.css';
 import { Link } from "react-router-dom";
 import { HiBars3CenterLeft } from "react-icons/hi2";
 import { IoClose } from "react-icons/io5";
+import logo from '../../../assets/logos/logo.png';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
@@ -11,30 +12,31 @@ const Navbar = () => {
         <>
             <nav className='header'>
                 <Link to='/'>
-                    {/* <img src={logo} alt="Logo" /> */}Logo
+                    <img src={logo} style={{ height: '44px' }} alt="Logo" />
                 </Link>
-                <div>
-                    <ul id='navbar' className={open ? '#navbar active' : '#navbar'}>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/shop">Shop</Link>
-                        </li>
-                        <li>
-                            <Link to="/order">Order</Link>
-                        </li>
-                        <li>
-                            <Link className='button' to="/login">Login</Link>
-                        </li>
-                    </ul>
-                </div>
+                <ul id='navbar' className={open ? '#navbar active' : '#navbar'}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/events">Events</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li className='register-btn'>
+                        <Link to="/register">Register</Link>
+                    </li>
+                    <li className='admin-btn'>
+                        <Link to="/admin">Admin</Link>
+                    </li>
+                </ul>
                 <div id='mobile' onClick={() => setOpen(!open)}>
                     {
                         open ?
-                            <IoClose className='fa-bars' />
+                            <IoClose className="icon" />
                             :
-                            <HiBars3CenterLeft className='fa-bars' />
+                            <HiBars3CenterLeft className="icon" />
                     }
                 </div>
             </nav>

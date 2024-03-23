99c350eaef53db547ddc1016d07c345461408d1c
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-23 12:56:32 +0600

    update blog page task

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,92 +1,89 @@
 import { Accordion, AccordionDetails, AccordionSummary, Typography } from "@mui/material";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import blog from '../../assets/images/blog.gif';
 import './Blog.css';
 
 const Blog = () => {
     return (
         <section className="blog-section">
-            <div>
-                
+            <div className="blog-image">
+                <img src={blog} alt="blog" />
+            </div>
+            <div className="blog-info">
                 <Accordion defaultExpanded>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel1-content"
                         id="panel1-header"
                     >
-                        <Typography>Expanded by default</Typography>
+                        <Typography>What is a volunteer network?</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            Volunteering networks provide an opportunity for Volunteer Managers to share information and resources, discuss issues of common interest, access professional development and make new connections and collaborate on projects and activities. Networks can be based on location sector and purpose.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-
                 <Accordion>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel2-content"
                         id="panel2-header"
                     >
-                        <Typography>Question 2</Typography>
+                        <Typography>How do I create a volunteer network?</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            1. Conduct a community needs assessment. <br />
+                            2. Set goals for your volunteer program. <br />
+                            3. Craft a mission statement. <br />
+                            4. Create a leadership team. <br />
+                            5. Define your volunteer roles.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-
                 <Accordion>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel2-content"
                         id="panel2-header"
                     >
-                        <Typography>Question 3</Typography>
+                        <Typography>What is the work of volunteer?</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            Volunteering is a voluntary act of an individual or group freely giving time and labor, often for community service. Many volunteers are specifically trained in the areas they work, such as medicine, education, or emergency rescue.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-
                 <Accordion>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel2-content"
                         id="panel2-header"
                     >
-                        <Typography>Question 4</Typography>
+                        <Typography>Will volunteering make me happy?</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            Volunteering reduces stress and increases positive, relaxed feelings by releasing dopamine. By spending time in service to others, volunteers report feeling a sense of meaning and appreciation.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-
                 <Accordion>
                     <AccordionSummary
                         expandIcon={<ExpandMoreIcon />}
                         aria-controls="panel2-content"
                         id="panel2-header"
                     >
-                        <Typography>Question 5</Typography>
+                        <Typography>What is an example of volunteer experience?</Typography>
                     </AccordionSummary>
                     <AccordionDetails>
                         <Typography>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                            malesuada lacus ex, sit amet blandit leo lobortis eget.
+                            Volunteering can range from hands-on activities such as building homes, teaching, or providing medical care, to behind-the-scenes work like fundraising, administration, or marketing.
                         </Typography>
                     </AccordionDetails>
                 </Accordion>
-
             </div>
         </section>
     );

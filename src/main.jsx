10b1bad05be52f0092941cea483a5f42bb512f71
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-29 12:56:15 +0600

    added AuthProvider task

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider.jsx'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
